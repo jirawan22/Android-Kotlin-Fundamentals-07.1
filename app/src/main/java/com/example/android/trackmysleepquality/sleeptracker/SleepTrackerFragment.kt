c05e548d0184506bc0f256c88c7b7a416d115fd4
jirawan.bn <jirawan.bn@rmuti.ac.th>
2020-09-05 15:27:11 +0700

    7.1-4-Step 5: Get data into the adapter

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -110,6 +110,12 @@ class SleepTrackerFragment : Fragment() {
                 sleepTrackerViewModel.doneNavigating()
             }
         })
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
         return binding.root
 
     }
